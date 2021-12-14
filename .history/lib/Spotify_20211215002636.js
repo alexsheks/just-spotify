1529a430771068b4e1dfa6944ceda2bf08012353
alex.sheks <alex.sheks.1997@gmail.com>
2021-12-15 00:49:01 +0800

    first commit

---
+++ .history/lib/Spotify_20211215002636.js
@@ -0,0 +1,10 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const scopes = [
+    'user-read-email',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'streaming',
+    'user-read-private',
+    'user-library-read'
+];
