5efc35be288a740c207e866fbc1c42a8b885adba
alex.sheks <alex.sheks.1997@gmail.com>
2021-12-17 00:40:07 +0800

    final demo

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "2a4xUaCWZKwWR5hlpzHY9q",
+});
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
---
+++ pages/library.js
@@ -0,0 +1,5 @@
+function Library() {
+    return <div>YOYOYO</div>;
+}
+
+export default Library;
